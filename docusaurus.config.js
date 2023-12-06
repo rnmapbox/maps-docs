f297fdf3e98be2673cf4e692ed4731bbfb8dbc7c
Miklós Fazekas <mfazekas@szemafor.com>
2023-12-06 12:29:37 +0100

    chore: up docosaurus to v3

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,8 +1,9 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
 
-const lightCodeTheme = require('prism-react-renderer/themes/github');
-const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+const {themes} = require('prism-react-renderer');
+const lightTheme = themes.github;
+const darkTheme = themes.dracula;
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
@@ -128,8 +129,8 @@ const config = {
         /* copyright: `Copyright © ${new Date().getFullYear()}. All trademarks and copyrights belong to their respective owners.`, */
       },
       prism: {
-        theme: lightCodeTheme,
-        darkTheme: darkCodeTheme,
+        theme: lightTheme,
+        darkTheme: darkTheme,
       },
       algolia: {
         appId: 'DSWU223Y1Q',
